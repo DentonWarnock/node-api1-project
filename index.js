2032ba789eed64055deee40e27fb114c2b6e008c
DentonWarnock <dentonwarnock@gmail.com>
2020-01-15 09:18:25 -0800

    initial commit, server running on port 443

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.listen(443, () => {
+  console.log("*** listening on port 443");
+});
+
+//middleware
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("hello world!");
+});
