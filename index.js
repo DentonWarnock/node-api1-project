3385e6f3cd54041b6ac850d76015a915ac673a0c
DentonWarnock <dentonwarnock@gmail.com>
2020-01-15 10:22:21 -0800

    get user by id endpoint working

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-// creates new user using info sent inside req.body
+// POST new user - creates new user using the info sent inside req.body
 server.post("/api/users", (req, res) => {
   const userInfo = req.body;
 
@@ -29,7 +29,53 @@ server.post("/api/users", (req, res) => {
         res.status(201).json({ success: true, user });
       })
       .catch(err => {
-        res.status(500).json({ success: false, err });
+        res.status(500).json({
+          success: false,
+          errorMessage:
+            "There was an error while saving the user to the database"
+        });
       });
   }
 });
+
+// GET users - Returns an array of all the user objects contained in the database.
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json({ success: true, users });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+// GET users/:id - Returns the user object with the specified id.
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json({ success: true, user });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        errorMessage: "The user information could not be retrieved."
+      });
+    });
+});
+
+// DELETE users/:id - Removes the user with the specified id and returns the deleted user.
+
+// PUT users/:id - Updates the user with the specified id using data from the request body.
+// Returns the modified document, NOT the original.
